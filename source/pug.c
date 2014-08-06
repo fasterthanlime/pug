68f5c6f4a104231f3c85a8a76f4ee1b46d32c7b9
Amos Wenger <fasterthanlime@gmail.com>
2014-08-06 11:24:01 +0200

    libcollection's vector seems to work nicely.

--- source/pug.c
+++ source/pug.c
@@ -1,16 +1,40 @@
 
 #include "parser.h"
+#include <libcollections/vector.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct operation {
+  char op;
+  int l;
+  int r;
+} operation_t;
+
+DECLARE_VECTOR_TYPE   ( operation, operation_t )
+IMPLEMENT_VECTOR_TYPE ( operation, operation_t )
+
+static vector_operation_t collection;
+
 int main() {
-    pug_parser_context_t *ctx = pug_parser_create(NULL);
-    while (pug_parser_parse(ctx, NULL));
-    pug_parser_destroy(ctx);
-    return 0;
+  vector_operation_create(&collection, 1);
+
+  pug_parser_context_t *ctx = pug_parser_create(NULL);
+  while (pug_parser_parse(ctx, NULL));
+  pug_parser_destroy(ctx);
+
+  printf("Replaying...\n");
+  for (int i = 0; i < vector_operation_size(&collection); i++) {
+    operation_t *operation = vector_operation_get(&collection, i);
+    printf("Computed %d %c %d\n", operation->l, operation->op, operation->r);
+  }
+
+  return 0;
 }
 
 void pug_parser_on_operation(char op, int l, int r) {
-    printf("Computing %d %c %d\n", l, op, r);
+  operation_t *operation = vector_operation_pushx(&collection);
+  operation->op = op;
+  operation->l = l;
+  operation->r = r;
 }
 
